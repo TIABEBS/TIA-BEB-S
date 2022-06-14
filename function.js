bb261c448fa03e862063d0fe1f7f7eaa2f8694b4
TIABEBS <107485009+TIABEBS@users.noreply.github.com>
2022-06-14 22:46:53 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,39 @@
+function facebook(){
+    window.location.href="https://web.facebook.com/tiabebscoffeeandtea";
+}
+
+function drinks(){
+    window.location.href="drinks.html";
+}
+
+function others(){
+    window.location.href="others.html";
+}
+
+function drinks2a(){
+    document.getElementById("a").src = "drinks/e.jpg";
+    document.getElementById("b").src = "drinks/f.jpg";
+    document.getElementById("c").src = "drinks/g.jpg";
+    document.getElementById("d").src = "drinks/h.jpg";
+}
+
+function drinks2b(){
+    document.getElementById("a").src = "drinks/a.jpg";
+    document.getElementById("b").src = "drinks/b.jpg";
+    document.getElementById("c").src = "drinks/c.jpg";
+    document.getElementById("d").src = "drinks/d.jpg";
+}
+
+function others2a(){
+    document.getElementById("e").src = "others/m.jpg";
+    document.getElementById("f").src = "others/n.jpg";
+    document.getElementById("g").src = "others/o.jpg";
+    document.getElementById("h").src = "others/p.jpg";
+}
+
+function others2b(){
+    document.getElementById("e").src = "others/i.jpg";
+    document.getElementById("f").src = "others/j.jpg";
+    document.getElementById("g").src = "others/k.jpg";
+    document.getElementById("h").src = "others/l.jpg";
+}
